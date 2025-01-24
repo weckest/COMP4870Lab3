0f068d9b497c8330f1d330a5e0e006d37d2653c6
Logan Pedersen <lpedersen15@my.bcit.ca>
2025-01-24 14:25:25 -0800

    tiny change to the program.cs

--- Program.cs
+++ Program.cs
@@ -38,4 +38,11 @@ app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
 
+   using (var scope = app.Services.CreateScope()) {
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationDbContext>();    
+    context.Database.Migrate();
+}
+
 app.Run();
